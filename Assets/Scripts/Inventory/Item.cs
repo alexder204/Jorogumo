922afe3c5cd8b42fac8ecda39d475f3266515247
Alexander Derderian <alexander.derderian@dawsoncollege.qc.ca>
2025-05-01 15:19:21 -0400

    added assemblies

--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
-    public string itemName;  // Name of the item
-    public Sprite icon;  // Icon for UI representation
-    public int stackAmount = 10;  // Maximum stackable amount
-    public int currentAmount = 0;  // Current stack in the inventory
-    public bool isUsable = false;  // Determines if the item can be used (e.g., consumable)
+    public string id;  // <-- Add this!
+    public string itemName;
+    public Sprite icon;
+    public int stackAmount = 10;
+    public int currentAmount = 0;
+    public bool isUsable = false;
 }
